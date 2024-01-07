512e5490461012e4e712a6a4e59b1754a0fe2922
pabcrudel <pabcrudel@gmail.com>
2024-01-07 20:58:36 +0100

    add: Create a way to build MPAs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useState } from 'react';
 import Footer from './components/Footer';
 
 export default function App () {
+  const [currentPath] = useState(window.location.pathname);
+
+  return (
+    <>
+      {
+        currentPath === '/'
+          ? <HomePage/>
+          : currentPath === '/about'
+            ? <AboutPage/>
+            : <NotFoundPage/>
+      }
+
+      <Footer repositoryName='react-router-clone'/>
+    </>
+  );
+}
+
+function HomePage () {
   return (
     <>
       <header>
@@ -9,9 +28,43 @@ export default function App () {
 
       <main>
         <p>A React library that enables <strong>client side routing</strong></p>
-      </main>
 
-      <Footer repositoryName='react-router-clone'/>
+        <a href="/about">Go to about page</a>
+      </main>
+    </>
+  );
+}
+
+function AboutPage () {
+  return (
+    <>
+      <header>
+        <h1>About this project</h1>
+      </header>
+
+      <main>
+        <p>
+          I&apos;m Pablo Cru and I&apos;m following Midudev&apos;s React Crash
+          Curse in order to learn React, How React Router works and how to
+          publish my own library
+        </p>
+
+        <a href="/">Go to home page</a>
+      </main>
+    </>
+  );
+}
+
+function NotFoundPage () {
+  return (
+    <>
+      <header>
+        <h1>404</h1>
+      </header>
+
+      <main>
+        <p>There&apos;s nothing here</p>
+      </main>
     </>
   );
 }
