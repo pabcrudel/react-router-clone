937e88481c7dff59e0108bc2eba7f4dfb35864a1
pabcrudel <pabcrudel@gmail.com>
2024-01-07 20:14:05 +0100

    add: Initiate a Vite + React project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App () {
+  return (
+    <>
+      <header>
+        <h1>React Router Clone</h1>
+      </header>
+
+      <main>
+        <p>A React library that enables <strong>client side routing</strong></p>
+      </main>
+    </>
+  );
+}
