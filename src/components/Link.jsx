41481056b66c080cfa94d9ea1557595ed15e26ac
pabcrudel <pabcrudel@gmail.com>
2024-01-08 13:18:51 +0100

    feature: Allow keyboard modifier events

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -2,16 +2,13 @@ import { NAVIGATION_EVENT } from '../constants';
 import PropTypes from 'prop-types';
 
 export default function Link ({ target, to, ...props }) {
-  const handleClick = event => {
-    // Avoid page refreshing when the anchor is clicked
-    event.preventDefault();
-
-    // Perform navigation
-    navigate(to);
-  };
-
   return (
-    <a onClick={handleClick} href={to} target={target} {...props}></a>
+    <a
+      onClick={event => navigationManager(event, target, to)}
+      href={to}
+      target={target}
+      {...props}
+    ></a>
   );
 }
 
@@ -19,6 +16,33 @@ Link.propTypes = {
   target: PropTypes.string,
   to: PropTypes.string.isRequired
 };
+
+/** Event management is the same as React Router original library because it
+ * is a common way to do so.
+ * @see [Source: React Router](https://github.com/remix-run/react-router/blob/main/packages/react-router-dom/dom.ts)
+*/
+function navigationManager (event, target, to) {
+  /** Button 0 is the primary click. Usually it is the left click */
+  const isMainEvent = event.button === 0;
+
+  /** If any keyboard key that modifies the event is clicked */
+  const isModifiedEvent =
+    event.metaKey ||
+    event.altKey ||
+    event.ctrlKey ||
+    event.shiftKey;
+
+  /** If new page must be opened in the same window */
+  const isManageableEvent = !target || target === '_self';
+
+  if (isMainEvent && isManageableEvent && !isModifiedEvent) {
+    // Avoid page refreshing when the anchor is clicked
+    event.preventDefault();
+
+    // Perform navigation
+    navigate(to);
+  }
+}
 
 function navigate (href) {
   window.history.pushState({}, '', href);
